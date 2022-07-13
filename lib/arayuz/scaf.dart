5ed6d60bfe9498c9032706055de57accda9451dc
Vedat Biner <vbiner@gmail.com>
2022-07-13 16:40:00 +0300

    Gridview eklendi

--- lib/arayuz/scaf.dart
+++ lib/arayuz/scaf.dart
@@ -4,6 +4,7 @@ class ScaffoldOgesi extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo.shade100,
       appBar: AppBar(
         title: const Text("My Fancy Dress"),
         actions: <Widget>[
@@ -24,8 +25,235 @@ class ScaffoldOgesi extends StatelessWidget{
           ),
         ],
       ),
-      body: const Center(
-        child: Text("Merhaba Scaffold"),
+      body: SafeArea(
+        child: Center(
+          child: GridView.count(
+            primary: false,
+            padding: const EdgeInsets.all(20.0),
+            crossAxisSpacing: 10.0,
+            crossAxisCount: 2,
+            children: [
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+              Container(
+                color: Colors.blue.shade400,
+                margin: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: const Text("Grid Elemanı-01", textAlign: TextAlign.center,),
+              ),
+            ],
+          ),
+          // child: Text("Merhaba Scaffold"),
+        ),
+
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
@@ -42,7 +270,32 @@ class ScaffoldOgesi extends StatelessWidget{
             label: "Değerlendirme",
           ),
         ],
-        onTap: (int i ) => debugPrint("Tıklanan butonun indeksi : $i"),
+        onTap: (int i ) {
+          switch(i){
+            case 0:
+              debugPrint("Tıklanan buton : Arşiv");
+              break;
+            case 1:
+              debugPrint("Tıklanan buton : Bilgilendirme");
+              break;
+            case 2:
+              debugPrint("Tıklanan buton : Değerlendirme");
+              break;
+            default:
+              debugPrint("Tıklanan buton bulunamadı");
+              break;
+          }
+        },
+      ),
+      drawer: Drawer(
+        child: ListTile(
+          leading: const Icon(Icons.change_history),
+          title: const Text("Change History"),
+          onTap: (){
+            Navigator.pop(context);
+            debugPrint("Yan menü tıklandı");
+          },
+        ),
       ),
     );
   }
