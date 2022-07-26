20a6bf1d913cf33024bc4aedd0ae84e555394afc
Vedat Biner <vbiner@gmail.com>
2022-07-26 16:27:33 +0300

    Animation-1

--- lib/pages/bilgilendirme.dart
+++ lib/pages/bilgilendirme.dart
@@ -12,9 +12,58 @@ class BilgilendirmeSayfasi extends StatelessWidget {
       body: Container(
         color: Colors.greenAccent,
         child: const Center(
-          child: Text("Bilgilendirme Sayfası"),
+          child: YaziHareket(),
         ),
       ),
     );
   }
 }
+
+class YaziHareket extends StatefulWidget {
+  const YaziHareket({Key? key}) : super(key: key);
+
+  @override
+  State<YaziHareket> createState() => _YaziHareketState();
+}
+
+class _YaziHareketState extends State<YaziHareket> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  int durum = 0;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this // vsync : ekran içindeki hareketleri takip ediyor.
+    );
+    _animation = CurvedAnimation(
+        parent: _controller,
+        curve: Curves.bounceInOut)
+          ..addListener(() => setState(() {}))
+          ..addStatusListener((durum){
+            debugPrint(durum.toString());
+          }
+        );
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Bilgilendirme Sayfası",
+        style: TextStyle(
+          fontSize: 8 + _animation.value * 25,
+        ),
+      ),
+    );
+  }
+}
+
